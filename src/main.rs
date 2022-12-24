fa26be67cc452a8999e3f0a810722519a9ab962e
Genevieve Zenla <gen@nyble.dev>
2022-12-24 02:39:06 -0600

    that's not what I meant to do, but it's pretty cool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs::File;
+
+use gifed::{
+	block::{extension::GraphicControl, Palette},
+	writer::{ImageBuilder, Writer},
+	Color,
+};
+
 fn main() {
-    println!("Hello, world!");
+	let file = File::create("screento.gif").unwrap();
+	let mut write = Writer::new(file, 256, 144, Some(grayscale())).unwrap();
+
+	let mut buffer = vec![0u8; 256 * 144];
+	for x in 0u8..=255 {
+		for y in 0..144 {
+			buffer[x as usize + y as usize * 144] = 1; //x;
+		}
+
+		write
+			.image(
+				ImageBuilder::new(256, 144)
+					.delay(2)
+					.build(buffer.clone())
+					.unwrap(),
+			)
+			.unwrap();
+	}
+
+	write.done().unwrap();
+}
+
+pub fn grayscale() -> Palette {
+	let mut plt = Palette::new();
+
+	plt.push(Color::new(0, 0, 0));
+	plt.push(Color::new(255, 255, 255));
+	/*for idx in 0..=255 {
+		plt.push(Color::new(idx, idx, idx));
+	}*/
+
+	plt
 }
