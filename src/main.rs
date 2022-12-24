88621ea7393201038547b25236e350f55b96ad8c
Genevieve Zenla <gen@nyble.dev>
2022-12-24 03:06:42 -0600

    'hello, world!'

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,61 @@
 use std::fs::File;
 
+use fontster::{parse_font_file, Layout, LayoutSettings, StyledText};
 use gifed::{
-	block::{extension::GraphicControl, Palette},
+	block::{
+		extension::{DisposalMethod, GraphicControl},
+		Palette,
+	},
 	writer::{ImageBuilder, Writer},
 	Color,
 };
 
 fn main() {
-	let file = File::create("screento.gif").unwrap();
-	let mut write = Writer::new(file, 256, 144, Some(grayscale())).unwrap();
+	let font = parse_font_file("LTCarpet.ttf").unwrap();
+	let mut layout: Layout<()> = Layout::new(LayoutSettings::default());
+	layout.append(
+		&[&font],
+		StyledText {
+			text: "hello, world!",
+			font_size: 64.0,
+			font_index: 0,
+			user: (),
+		},
+	);
 
-	let mut buffer = vec![0u8; 256 * 144];
-	for x in 0u8..=255 {
-		for y in 0..144 {
-			buffer[x as usize + y as usize * 256] = x;
+	let padding = layout.height() / 2.0;
+	let half_pad = (padding / 2.0) as u16;
+	let width = layout.width() + padding;
+	let height = layout.height() + padding * 5.0;
+
+	let file = File::create("screento.gif").unwrap();
+	let mut write = Writer::new(file, width as u16, height as u16, Some(grayscale())).unwrap();
+
+	let glyphs = layout.glyphs();
+	let mut last_space = false;
+	for glyph in glyphs {
+		if glyph.c.is_whitespace() {
+			last_space = true;
+			continue;
 		}
 
+		let delay = if last_space { 20 } else { 10 };
+
+		let (_, raster) = font.rasterize(glyph.c, glyph.font_size);
 		write
 			.image(
-				ImageBuilder::new(256, 144)
-					.delay(2)
-					.build(buffer.clone())
+				ImageBuilder::new(glyph.width as u16, glyph.height as u16)
+					.offset(
+						glyph.x.round() as u16 + half_pad,
+						glyph.y.round() as u16 + half_pad * 5,
+					)
+					.delay(delay)
+					.build(raster)
 					.unwrap(),
 			)
 			.unwrap();
+
+		last_space = false;
 	}
 
 	write.done().unwrap();
