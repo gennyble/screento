0964d3efa278fae755b93d05b058942a96fed4e0
Genevieve Zenla <gen@nyble.dev>
2022-12-24 05:46:53 -0600

    done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod count;
 
-use std::{borrow::Borrow, fs::File, time::Instant};
+use std::{borrow::Borrow, fs::File, path::Path, time::Instant};
 
 use fontster::{
 	parse_font_file, Font, HorizontalAlign, Layout, LayoutSettings, LineHeight, StyledText,
@@ -18,10 +18,6 @@ use crate::count::{Count, Number};
 
 fn main() {
 	let font = parse_font_file("Instruction.otf").unwrap();
-	let mut layout: Layout<()> = Layout::new(LayoutSettings {
-		horizontal_align: HorizontalAlign::Center,
-		line_height: LineHeight::Smallest(0.0),
-	});
 
 	let all_start = Instant::now();
 
@@ -43,46 +39,6 @@ fn main() {
 		m_metrics.height
 	);
 
-	// The spacing between letters
-	let spacing = widest.width as f32 / 4.0;
-	// The padding we put around the edges of the image
-	let edge_padding = spacing * 2.0;
-	// The spacing between groups. Like in 1 000 000 the groups are 1, 000, and 000
-	let group_padding = spacing * 2.0;
-
-	// In the number 1_000_000 there are three groups.
-	// So we only beed 2 group_padding.
-	// In all the groups, there is only ever two characters directly next
-	// to one another four times. 4 spacing.
-	let width =
-		widest.width as f32 * 7.0 + spacing * 4.0 + group_padding * 2.0 + edge_padding * 2.0;
-	let height = widest.height as f32 + edge_padding * 2.0;
-
-	// Could I make this automated and work with infinetly large numbers? Probably.
-	// Do I want to do that right now? No.
-
-	// Width from the left, no padding
-	let wnp = width - edge_padding;
-	let gw = widest.width as f32;
-	// Half glyph width
-	let hgw = widest.width as f32 / 2.0;
-
-	// Positions ones, tenths, hundreds
-	let positions = vec![
-		wnp - hgw,
-		wnp - gw - spacing - hgw,
-		wnp - gw * 2.0 - spacing * 2.0 - hgw,
-		wnp - gw * 3.0 - spacing * 2.0 - group_padding - hgw,
-		wnp - gw * 4.0 - spacing * 3.0 - group_padding - hgw,
-		wnp - gw * 5.0 - spacing * 4.0 - group_padding - hgw,
-		wnp - gw * 6.0 - spacing * 4.0 - group_padding * 2.0 - hgw,
-	];
-
-	let mut count = Count::new(positions);
-
-	let width = width.ceil() as usize;
-	let height = height.ceil() as usize;
-
 	// The below code- the very, very messy code- does this:
 	// get the raster from fontster (really from fontdue, but fontster rexports) and stuff
 	// it into a WidestXTallest buffer. The glyph is horizontally centered and vertically
@@ -103,17 +59,92 @@ fn main() {
 		numbers.push(n_buffer);
 	}
 
-	let file = File::create("1000.gif").unwrap();
+	let things = Things { numbers, widest };
+	doit(0, 100, "100.gif", &things);
+	doit(0, 1000, "1000.gif", &things);
+	doit(0, 10_000, "10_000.gif", &things);
+	doit(0, 100_000, "100_000.gif", &things);
+	doit(0, 1_000_000, "1_000_000.gif", &things);
+	println!(
+		"Everything is done! Took {:.2}s",
+		all_start.elapsed().as_secs_f32()
+	)
+}
+
+pub struct Things {
+	numbers: Vec<Vec<u8>>,
+	widest: Widest,
+}
+
+fn doit<P: AsRef<str>>(start: usize, the_number: usize, filename: P, things: &Things) {
+	let digits = num_digits(the_number);
+	let three_group = digits / 3;
+
+	let Things { numbers, widest } = things;
+
+	// The spacing between letters
+	let spacing = widest.width as f32 / 4.0;
+	// The padding we put around the edges of the image
+	let edge_padding = spacing * 2.0;
+	// The spacing between groups. Like in 1 000 000 the groups are 1, 000, and 000
+	let group_padding = spacing * 2.0;
+
+	let mut width = widest.width as f32 * digits as f32 + edge_padding * 2.0;
+	if three_group > 0 {
+		// Every group of three has two pairs of adjacent glyphs
+		width += spacing * three_group as f32 * 2.0;
+	}
+	if digits % 3 != 0 {
+		width += group_padding * three_group as f32;
+	}
+
+	let height = widest.height as f32 + edge_padding * 2.0;
+
+	// Width from the left, no padding
+	let wnp = width - edge_padding;
+	let gw = widest.width as f32;
+	// Half glyph width
+	let hgw = widest.width as f32 / 2.0;
+
+	// Positions ones, tenths, hundreds
+	let positions = vec![
+		wnp - hgw,
+		wnp - gw - spacing - hgw,
+		wnp - gw * 2.0 - spacing * 2.0 - hgw,
+		wnp - gw * 3.0 - spacing * 2.0 - group_padding - hgw,
+		wnp - gw * 4.0 - spacing * 3.0 - group_padding - hgw,
+		wnp - gw * 5.0 - spacing * 4.0 - group_padding - hgw,
+		wnp - gw * 6.0 - spacing * 4.0 - group_padding * 2.0 - hgw,
+	];
+
+	let mut count = Count::new(positions);
+	count.set(start);
+
+	let width = width as usize;
+	let height = height as usize;
+
+	let filename = filename.as_ref();
+	let file = File::create(filename).unwrap();
 	let mut write = Writer::new(file, width as u16, height as u16, Some(grayscale())).unwrap();
 
-	/*write
-	.block(Block::CommentExtension(
-		String::from("by gennyble").as_bytes().to_vec(),
-	))
-	.unwrap();*/
+	write
+		.block(Block::CommentExtension(
+			String::from("by gennyble").as_bytes().to_vec(),
+		))
+		.unwrap();
+
+	// Since nothing seemingly respects our background colour and just makes it transparent,
+	// we're drawing a sea of black here.
+	write
+		.image(
+			ImageBuilder::new(width as u16, height as u16)
+				.build(vec![0; width * height])
+				.unwrap(),
+		)
+		.unwrap();
 
 	let gif_start = Instant::now();
-	for _ in 0..1_000_000 {
+	for _ in start..the_number {
 		let count_numbers = count.next();
 
 		let (last, count_numbers) = count_numbers.split_last().unwrap();
@@ -142,11 +173,7 @@ fn main() {
 			)
 			.unwrap()
 	}
-	println!(
-		"Gif took {}ms\nEverything took {}ms",
-		gif_start.elapsed().as_millis(),
-		all_start.elapsed().as_millis()
-	);
+	println!("{filename} took {}ms", gif_start.elapsed().as_millis());
 
 	write.done().unwrap();
 }
@@ -190,4 +217,9 @@ pub struct Widest {
 	pub width: usize,
 	pub tall_number: u8,
 	pub height: usize,
+}
+
+// https://stackoverflow.com/a/15587842
+pub fn num_digits(n: usize) -> usize {
+	((n as f32).log10() + 1.0) as usize
 }
